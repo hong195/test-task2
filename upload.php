e4c4195d083f31b71cf7ee0c8f0ff1b551e1f8f5
aleksejem <alexeyhong10@gmail.com>
2024-10-15 13:27:16 +0500

    Added resumable.php package.

--- upload.php
+++ upload.php
@@ -1,1 +1,24 @@
 <?php
+
+include 'vendor/autoload.php';
+
+
+include 'vendor/autoload.php';
+
+use Dilab\Network\SimpleRequest;
+use Dilab\Network\SimpleResponse;
+use Dilab\Resumable;
+
+$request = new SimpleRequest();
+$response = new SimpleResponse();
+
+$resumable = new Resumable($request, $response);
+
+$tempDir = __DIR__ . '/uploads/chunks';
+$targetDir = __DIR__ . '/uploads/';
+
+$resumable->tempFolder = $tempDir;
+$resumable->uploadFolder = $targetDir;
+
+$resumable->process();
+
